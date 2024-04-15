6351ca717780710058ca487dbda1040482c9312d
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-04-15 20:59:56 +0300

    update step2

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,21 @@
+package hexlet.code;
+
+public class App {
+    public static void main(String[] args) {
+        Validator val = new Validator();
+        StringSchema schem = val.string();
+        System.out.println(schem.isValid("hexlet"));
+        System.out.println(schem.isValid(""));
+        System.out.println(schem.isValid(null));
+
+        schem.required();
+        System.out.println("================");
+        System.out.println(schem.isValid("hexlet"));
+        System.out.println(schem.isValid(""));
+        System.out.println(schem.isValid(null));
+
+        boolean result = schem.minLength(3).contains("ma").isValid("mama");
+        System.out.println("1) " + result);
+
+    }
+}
