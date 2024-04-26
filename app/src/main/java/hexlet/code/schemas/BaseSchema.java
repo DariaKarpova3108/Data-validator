9582eee75543153767333ac8aa1add3065ce1cce
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-04-26 13:13:35 +0300

    update class - BaseSchema

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,15 +5,10 @@ import java.util.Map;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema<T> {
-    protected boolean isRequired = false;
     protected Map<String, Predicate<T>> checks;
 
     public BaseSchema() {
         this.checks = new LinkedHashMap<>();
-    }
-
-    public void setRequired(boolean required) {
-        isRequired = required;
     }
 
     protected final void addCheck(String check, Predicate<T> predicate) {
