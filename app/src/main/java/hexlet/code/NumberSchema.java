0b3a2fddcf1af41ff00fa41545a9135d1efb4013
DariaKarpova <110734901+DariaKarpova@users.noreply.github.com>
2024-05-02 14:21:43 +0300

    update logic in class NumberSchema

--- app/src/main/java/hexlet/code/NumberSchema.java
+++ app/src/main/java/hexlet/code/NumberSchema.java
@@ -5,8 +5,6 @@ import hexlet.code.schemas.BaseSchema;
 import java.util.Objects;
 
 public final class NumberSchema extends BaseSchema<Integer> {
-    private Integer[] diapazon = new Integer[2];
-
     public NumberSchema() {
     }
 
@@ -26,16 +24,12 @@ public final class NumberSchema extends BaseSchema<Integer> {
         return this;
     }
 
-    public NumberSchema range(Integer begin, Integer end) {
-        this.diapazon[0] = begin;
-        this.diapazon[1] = end;
+    public NumberSchema range(Integer min, Integer max) {
         addCheck("range", element -> {
             if (element == null) {
                 return true;
-            } else if (diapazon[0] != null && diapazon[1] != null) {
-                return element >= diapazon[0] && element <= diapazon[1];
             } else {
-                return false;
+                return element >= min && element <= max;
             }
         });
         return this;
